70c79a5aedc01111b0f9af4ef0a88934c937ad2a
Zpewg <mstefanandrei129@gmail.com>
2025-04-25 16:17:09 +0300

    Completed the game

--- Labirint1/Server/Server.cs
+++ Labirint1/Server/Server.cs
@@ -28,8 +28,20 @@ public class Server
                 NetworkStream stream = client.GetStream();
                 StreamReader reader = new StreamReader(stream);
                 StreamWriter writer = new StreamWriter(stream);
-              
+              writer.AutoFlush = true;
                 SendMatrix(labirnth, writer);
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!checkDirection(line, ref labirnth, writer))
+                    {
+                        writer.WriteLine("Invalid input!");
+                        
+                    }
+
+                }
+
             }
         }
         catch (Exception e)
@@ -50,7 +62,7 @@ public class Server
                 line += hiddenMatrix[i, j] + " ";
             }
             streamWriter.WriteLine(line);
-            Console.WriteLine($"Sent: {line}");
+            
         }
         streamWriter.WriteLine("End");
         streamWriter.Flush();
@@ -59,20 +71,24 @@ public class Server
 
     public string[,] HideMatrix(string[,] labirnth)
     {
-        string[,] result = labirnth;
+       int rows = labirnth.GetLength(0);
+       int cols = labirnth.GetLength(1);
+       string[,] result = new string[rows, cols];
         for (int i = 0; i < labirnth.GetLength(0); i++)
         {
             for (int j = 0; j < labirnth.GetLength(1); j++)
             {
-                if (result[i, j] != "1")
+                result[i, j] = "1";
+                if (labirnth[i, j] != "1")
                 {
                     result[i, j] = "0";
                 }
+                
             }
         }
         return result;
     }
-    public void DisplayMatrix(string[,] labirinth)
+    public static void DisplayMatrix(string[,] labirinth)
     {
       
         for (int i = 0; i < labirinth.GetLength(0); i++)
@@ -83,5 +99,168 @@ public class Server
                 Console.Write(labirinth[i, j] + " ");
             }
         }
+    }
+
+    public static bool checkDirection(string direction, ref string[,] labirnth, StreamWriter streamWriter)
+    {
+        switch (direction.ToLower())
+        {
+            case "up": MoveUp(ref labirnth, streamWriter);
+                break; 
+            case "down": MoveDown(ref labirnth,streamWriter );
+                break;
+            case "left": MoveLeft(ref labirnth, streamWriter); 
+                break;
+            case "right": MoveRight(ref labirnth, streamWriter); 
+                break;
+            default : return false;
+        }
+        return true;
+    }
+
+    public static void MoveUp(ref string[,] labirnth, StreamWriter streamWriter)
+    {
+        
+        for (int i = 0; i < labirnth.GetLength(0)-1; i++)
+        {
+            for (int j = 0; j < labirnth.GetLength(1); j++)
+            {
+                
+                if (labirnth[i, j] == "1")
+                {
+                   
+                    if ( labirnth[i - 1, j] != "0")
+                    {
+                        string temp = labirnth[i - 1, j];
+                        CheckWinningCondition(temp, streamWriter);
+                        labirnth[i - 1, j] = "1"; 
+                        labirnth[i, j] = temp;
+                        string position = (i-1).ToString() + "," + j.ToString();
+                        Console.WriteLine(position);
+                        streamWriter.WriteLine(position);
+                        streamWriter.Flush();
+                        break;
+                    }
+                    
+                    streamWriter.WriteLine("You hit a wall!");
+                    streamWriter.Flush();
+                    break;
+                }
+            }
+        }
+        DisplayMatrix(labirnth);
+    }
+
+    public static void MoveDown(ref string[,] labirnth, StreamWriter streamWriter)
+    {
+        bool moved = false;
+        for (int i = 0; i < labirnth.GetLength(0)-1; i++)
+        {
+            for (int j = 0; j < labirnth.GetLength(1); j++)
+            {
+                
+                if (labirnth[i, j] == "1")
+                {
+                   
+                    if ( labirnth[i + 1, j] != "0" && !moved)
+                    {
+                        string temp = labirnth[i + 1, j];
+                        CheckWinningCondition(temp, streamWriter);
+                        labirnth[i + 1, j] = "1"; 
+                        labirnth[i, j] = temp;
+                        string position = (i+1).ToString() + "," + j.ToString();
+                        Console.WriteLine(position);
+                        streamWriter.WriteLine(position);
+                        streamWriter.Flush();
+                        
+                        moved = true;
+                        break;
+                    }
+                    
+                    streamWriter.WriteLine("You hit a wall!");
+                    streamWriter.Flush();
+                    break;
+                }
+            }
+            if(moved) break;
+        }
+        DisplayMatrix(labirnth);
+    }
+
+    public static void MoveLeft(ref string[,] labirnth, StreamWriter streamWriter)
+    {
+        for (int i = 0; i < labirnth.GetLength(0); i++)
+        {
+            for (int j = 0; j < labirnth.GetLength(1); j++)
+            {
+                
+                if (labirnth[i, j] == "1")
+                {
+                   
+                    if ( labirnth[i, j-1] != "0")
+                    {
+                        string temp = labirnth[i, j-1];
+                        CheckWinningCondition(temp, streamWriter);
+                        labirnth[i, j-1] = "1"; 
+                        labirnth[i, j] = temp;
+                        string position = i.ToString() + "," + (j-1).ToString();
+                        Console.WriteLine(position);
+                        streamWriter.WriteLine(position);
+                        streamWriter.Flush();
+                        break;
+                    }
+
+                    
+                   streamWriter.WriteLine("You hit a wall!");
+                   streamWriter.Flush();
+                   break;
+                }
+            }
+        }
+        DisplayMatrix(labirnth);
+    }
+
+    public static void MoveRight(ref string[,] labirnth, StreamWriter streamWriter)
+    {
+        for (int i = 0; i < labirnth.GetLength(0); i++)
+        {
+            for (int j = 0; j < labirnth.GetLength(1); j++)
+            {
+                
+                if (labirnth[i, j] == "1")
+                {
+                   
+                    if (labirnth[i, j+1] != "0")
+                    {
+                        string temp = labirnth[i, j+1];
+                        CheckWinningCondition(temp, streamWriter);
+                        labirnth[i, j+1] = "1"; 
+                        labirnth[i, j] = temp;
+                        string position = i.ToString() + "," + (j+1).ToString();
+                        Console.WriteLine(position);
+                        streamWriter.WriteLine(position);
+                        streamWriter.Flush();
+                        break;
+                    }
+                    
+                    streamWriter.WriteLine("You hit a wall!");
+                    streamWriter.Flush();
+                    break;
+                }
+            }
+        }
+        DisplayMatrix(labirnth);
+    }
+
+    public static void CheckWinningCondition(string pos, StreamWriter streamWriter)
+    {
+        if (pos == "3")
+        {
+            streamWriter.WriteLine("Found the exit! You win!");
+            streamWriter.Flush();
+            
+        }
+
+        
     }
 }
--- LabirinthClient1/Client.cs
+++ LabirinthClient1/Client.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using System.Data;
+using System.Net.Sockets;
 
 namespace LabirinthClient1;
 
@@ -19,27 +20,69 @@ public class Client
             using StreamReader streamReader = new StreamReader(stream);
 
             streamWriter.AutoFlush = true;
-          
-            
-            int lineCounter = 0;
+            PrintRules();
+            bool running = true;
+            while (running)
+            {
+                string line = Console.ReadLine();
+                switch (line)
+                {
+                    case "1": Console.WriteLine("Good luck!");
+                        running = false;
+                        break;
+                    case "0": Console.WriteLine("Good bye!");
+                        return;
+                    default: Console.WriteLine("Not a possible funciton"); break;
+                }
+            }
             while (true)
             {
                 
                 string line = streamReader.ReadLine();
                 if (line == "End")
                 {
-                    Console.WriteLine("Exiting...");
                     break;
                 }
                 
                 string[] row = line.Split(' ');
                 matrix.Add(row);
             }
-            Console.WriteLine("The matrix has" + matrix.Count + " elements");
-            foreach (var line in matrix)
-            {
-                Console.WriteLine(string.Join(' ', line));
-            }
+            
+           string[,] matrix2D = ListToMatrix(matrix);
+           PrintMatrix(matrix2D);
+           string messageFromServer;
+           while (true)
+           {
+               Console.WriteLine("\nWhat is your move?");
+               string message = Console.ReadLine();
+               streamWriter.WriteLine(message);
+               
+               messageFromServer = streamReader.ReadLine();
+               if (messageFromServer == "Found the exit! You win!")
+               {
+                   Console.WriteLine("Congratulations! You win!");
+                   break;
+               }
+               if (messageFromServer == "You hit a wall!")
+               {
+                   Console.WriteLine("You hit a wall!");
+                   continue;
+               }
+
+               if (messageFromServer == "Invalid input!")
+               {
+                   Console.WriteLine("Invalid input!");
+                   continue;
+               }
+               string[] coordinates = messageFromServer.Split(',');
+               int newRow = int.Parse(coordinates[0]);
+               int newCol = int.Parse(coordinates[1]);
+           
+               Console.WriteLine(newRow + "," + newCol);
+               MatrixUpdatedValues(ref matrix2D, newRow, newCol);
+               
+               
+           }
 
         }
         catch (Exception ex)
@@ -48,5 +91,61 @@ public class Client
         }
         Console.WriteLine("Press any key to continue...");
         Console.ReadKey();
+    }
+
+    public static void PrintRules()
+    {
+        Console.WriteLine("Hello and Welcome to the Labyrinth Game!"
+                          +"\n The rules are simple 1 represents your postion and you have to find the exit"
+                          +"\n Type 'up' to go up"
+                          +"\n Type 'down' to go down"
+                          +"\n Type 'left' to go left"
+                          +"\n Type 'right' to go right"
+                          +"\n Type 1 if you wish to continue ore 0 to leave");
+    }
+    public static void PrintMatrix(string[,] matrix2D)
+    {
+        for (int i = 0; i < matrix2D.GetLength(0); i++)
+        {
+            Console.WriteLine(" ");
+            for (int j = 0; j < matrix2D.GetLength(1); j++)
+            {
+                Console.Write(matrix2D[i, j] + " ");
+            }
+        }
+    }
+
+    public static void MatrixUpdatedValues(ref string[,] matrix, int row, int collumn)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == "1")
+                {
+                    matrix[i, j] = "0";
+                    matrix[row, collumn] = "1";
+               
+                }
+            }
+        }
+        PrintMatrix(matrix);
+     
+    }
+
+    public static string[,] ListToMatrix(List<string[]> matrix)
+    {
+        int rows = matrix.Count;
+        int cols = matrix[0].Length;
+        string[,] matrix2D = new string[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix2D[i, j] = matrix[i][j];
+            }
+        }
+        return matrix2D;
     }
 }
